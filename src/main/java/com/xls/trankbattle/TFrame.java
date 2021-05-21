1de9c15022354479ba7ea352c26f7b23d4cd302e
xiaolongren <52064463+Little-Dragon-God@users.noreply.github.com>
2021-05-21 21:51:03 +0800

    控制坦克方向

--- src/main/java/com/xls/trankbattle/TFrame.java
+++ src/main/java/com/xls/trankbattle/TFrame.java
@@ -10,10 +10,16 @@ import java.awt.event.WindowEvent;
 public class TFrame extends JFrame {
     int x = 200;
     int y = 200;
+    //坦克朝向
+    boolean bL = false;
+    boolean bR = false;
+    boolean bU = false;
+    boolean bD = false;
+
     //构造器
-    public TFrame(){
+    public TFrame() {
         setTitle("坦克大战");
-        setSize(800,600);
+        setSize(800, 600);
         //窗口不可改变大小
         setResizable(false);
         //窗口可见
@@ -21,12 +27,12 @@ public class TFrame extends JFrame {
         //添加键盘监听事件
         this.addKeyListener(new MyKeyListener());
         //关闭窗口
-       addWindowListener(new WindowAdapter() {
-           @Override
-           public void windowClosing(WindowEvent e) {
-               System.exit(0);
-           }
-       });
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 
     //画笔
@@ -34,17 +40,33 @@ public class TFrame extends JFrame {
     public void paint(Graphics g) {
         super.paint(g);
         //画矩形
-        g.fillRect(x,y,50,50);
-          x += 10;
+        g.fillRect(x, y, 50, 50);
+        x += 10;
 //        y += 10;
 
     }
+
     //键盘监听内部类
-    class MyKeyListener extends KeyAdapter{
+    class MyKeyListener extends KeyAdapter {
         //按下有效
         @Override
         public void keyPressed(KeyEvent e) {
-            x += 200;
+            //控制方向，默认不开启方向,按下开启
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+            }
 
         }
 
@@ -52,6 +74,22 @@ public class TFrame extends JFrame {
         @Override
         public void keyReleased(KeyEvent e) {
             super.keyReleased(e);
+            //控制方向，默认不开启方向,按下开启
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+            }
         }
     }
 }
